c21fb95f6e633fb77c6ebb0a3943cb1275d563bc
Bonamichi <marco.bom.amigo@gmail.com>
2018-07-09 13:16:45 -0300

    Importig the dataset

--- 1 - Data Preprocessing/data_preprocessing.py
+++ 1 - Data Preprocessing/data_preprocessing.py
@@ -1,4 +1,11 @@
+# Data Preprocessing
+
+# Importig the librares
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Importing the dataset
+dataset = pd.read_csv("data/Data.csv")
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
